59679712818ec39688c62f3b649acccce2e090de
unknown <ribeirojoaolucas68@gmail.com>
2022-04-21 02:44:50 -0300

    feat: import and add a image to the welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import wateringImg from "../assets/watering.png";
 
 export function Welcome() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Gerencie suas plantas de forma fácil</Text>
+      <Image source={wateringImg} />
+      <Text>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+});
