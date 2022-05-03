b6fb2d19a62c6c8d0853282fe5990a7ef92c0380
unknown <ribeirojoaolucas68@gmail.com>
2022-05-03 00:05:42 -0300

    feat: add the plant select page to the stack of routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import {
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 const stackRoutes = createNativeStackNavigator();
 
@@ -26,6 +27,10 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen 
             name="Confirmation"
             component={Confirmation}
+        />
+        <stackRoutes.Screen 
+            name="PlantSelect"
+            component={PlantSelect}
         />
     </stackRoutes.Navigator>
 );
