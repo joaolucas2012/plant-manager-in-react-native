9082a9053f623bdb84d59160db6b9c2362f27ced
unknown <ribeirojoaolucas68@gmail.com>
2022-05-09 18:04:16 -0300

    feat: add the plant card primary component

---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+    Text,
+    StyleSheet
+} from "react-native";
+import {
+    RectButton,
+    RectButtonProps
+} from "react-native-gesture-handler";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface PlantProps extends RectButtonProps{
+    data: {
+        name: string;
+        photo: string;
+    }
+}
+
+export function PlantCardPrimary({data, ...rest} : PlantProps){
+    return(
+        <RectButton 
+            style={styles.container}
+            {...rest}
+        >
+            <Text style={styles.text}>
+                {data.name}
+            </Text>
+        </RectButton>
+    );
+;}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        maxWidth: '45%',
+        backgroundColor: colors.shape,
+        borderRadius: 20,
+        paddingVertical: 10,
+        margin: 10,
+        alignItems: 'center',
+    },
+    text: {
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        marginVertical: 16,
+    },
+});
